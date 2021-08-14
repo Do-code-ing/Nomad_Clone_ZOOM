e40367e08e7b3456d899f76d22c7cf871421506f
Do-code-ing <zxcvbn658@naver.com>
2021-08-15 03:59:00 +0900

    2021-08-15

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => { // 서버가 작동했을 때
+    console.log("Connected to Server ✅");
+})
+
+socket.addEventListener("message", (message) => { // 서버로부터 메세지 받기
+    console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => { // 서버가 닫혔을 때
+    console.log("Disconnected from Server ❌")
+})
+
+setTimeout(() => { // 10초 뒤에 서버에게 메세지 보내기
+    socket.send("hello from the browser.");
+}, 1000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,8 +7,21 @@ const app = express();
 app.set("view engine", "pug"); // view engine 을 pug 로
 app.set("views", __dirname + "/views"); // express 의 템플릿이 어디 있는지 정의
 app.use("/public", express.static(__dirname + "/public")); // public url을 생성해서 유저가 접근할 수 있게
-app.get("/", (req, res) => res.render("home")); // home.pug 를 render 해주는 route handler 만들기
-app.get("/*", (req, res) => res.redirect("/")); // 다른 url 사용하게 하기, 지금은 다 home으로 보내기
+app.get("/", (_, res) => res.render("home")); // home.pug 를 render 해주는 route handler 만들기
+app.get("/*", (_, res) => res.redirect("/")); // 다른 url 사용하게 하기, 지금은 다 home으로 보내기
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
-app.listen(3000, handleListen);
+
+const server = http.createServer(app); // http 서버, 접근하기 위해
+const wss = new WebSocket.Server({server}); // websockets 서버, http 서버 위에 만들기, 2개의 protocol이 같은 port를 공유
+
+wss.on("connection", (socket) => { // 연결되었을 때, on == addEventListener?
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => console.log("Disconnected from Browser ❌")); // 브라우저와 연결이 끊기면
+    socket.on("message", message => { // 브라우저에게 메세지 받기
+        console.log(message.toString("utf-8"));
+    });
+    socket.send("hello"); // 커넥션이 발생했을 때, 브라우저에게 "hello" 라는 메세지 전달
+});
+
+server.listen(3000, handleListen);
