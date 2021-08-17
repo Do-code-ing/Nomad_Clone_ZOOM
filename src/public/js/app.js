961eea064642d4b9a7475f5b540143bd1ea1e70b
Do-code-ing <zxcvbn658@naver.com>
2021-08-17 17:52:38 +0900

    2021-08-17

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -84,6 +84,13 @@ function handleCameraClick() {
 
 async function handleCameraChange() {
     await getMedia(camerasSelect.value);
+    if (myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection
+            .getSenders()
+            .find((sender) => sender.track.kind = "video");
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
@@ -147,8 +154,10 @@ function makeConnection() {
     myPeerConnection = new RTCPeerConnection();
     myPeerConnection.addEventListener("icecandidate", handleIce);
     myPeerConnection.addEventListener("addstream", handleAddStream);
-    myStream.getTracks().forEach((track) =>
-        myPeerConnection.addTrack(track, myStream));
+    console.log(myStream);
+    myStream
+        .getTracks()
+        .forEach((track) => myPeerConnection.addTrack(track, myStream));
 }
 
 function handleIce(data) {
