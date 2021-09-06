163f50780f41f72f0e8c1127bdd780e44dcb18e1
Do-code-ing <zxcvbn658@naver.com>
2021-09-07 00:04:40 +0900

    2021-09-07

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -26,108 +26,108 @@ let myPeerConnection;
 let myDataChannel;
 
 async function getCamera() {
-    try {
-        const devices = await navigator.mediaDevices.enumerateDevices();
-        const cameras = devices.filter((device) => device.kind === "videoinput");
-        const currentCamera = myVideo.getVideoTracks()[0];
-        cameras.forEach((camera) =>{
-            const option = document.createElement("option");
-            option.value = camera.deviceId;
-            option.innerText = camera.label;
-            if (currentCamera.label === camera.label) {
-                option.selected = true;
-            }
-            camerasSelect.appendChild(option);
-        });
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myVideo.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 async function getVideo(deviceId) {
-    const initialVideo = {
-        video: { facingMode: "user" },
-    };
-    const constrainVideo = {
-        video: { deviceId: { exact: deviceId } },
-    };
-    try {
-        myVideo = await navigator.mediaDevices.getUserMedia(
-            deviceId ? constrainVideo : initialVideo
-        );
-        myFace.srcObject = myVideo;
-        if (!deviceId) {
-            await getCamera();
-        }
-    } catch (e) {
-        console.log(e);
-        myFace.style.backgroundImage = "url(public/img/unknown.png)";
-        myFace.style.backgroundSize = "cover";
-        cameraBtn.hidden = true;
-        camerasSelect.hidden = true;
+  const initialVideo = {
+    video: { facingMode: "user" },
+  };
+  const constrainVideo = {
+    video: { deviceId: { exact: deviceId } },
+  };
+  try {
+    myVideo = await navigator.mediaDevices.getUserMedia(
+      deviceId ? constrainVideo : initialVideo
+    );
+    myFace.srcObject = myVideo;
+    if (!deviceId) {
+      await getCamera();
     }
+  } catch (e) {
+    console.log(e);
+    myFace.style.backgroundImage = "url(public/img/unknown.png)";
+    myFace.style.backgroundSize = "cover";
+    cameraBtn.hidden = true;
+    camerasSelect.hidden = true;
+  }
 }
 
 async function getAudio() {
-    const initailAudio = {
-        audio: true,
-    };
-    try {
-        myAudio = await navigator.mediaDevices.getUserMedia(initailAudio);
-        myVoice.srcObject = myAudio;
-    } catch (e) {
-        console.log(e);
-        muteBtn.hidden = true;
-    }
+  const initailAudio = {
+    audio: true,
+  };
+  try {
+    myAudio = await navigator.mediaDevices.getUserMedia(initailAudio);
+    myVoice.srcObject = myAudio;
+  } catch (e) {
+    console.log(e);
+    muteBtn.hidden = true;
+  }
 }
 
 function handleMuteClick() {
-    if (myAudio) {
-        myAudio
-            .getAudioTracks()
-            .forEach((track) => (track.enabled = !track.enabled));
-        if (!muted) {
-            muteBtn.innerText = "Unmute";
-            muteBtn.style.backgroundColor = "darkslategray";
-            muteBtn.style.color = "white";
-            muted = true;
-        } else {
-            muteBtn.innerText = "Mute";
-            muteBtn.style.backgroundColor = "lightgray";
-            muteBtn.style.color = "black";
-            muted = false;
-        }
+  if (myAudio) {
+    myAudio
+      .getAudioTracks()
+      .forEach((track) => (track.enabled = !track.enabled));
+    if (!muted) {
+      muteBtn.innerText = "Unmute";
+      muteBtn.style.backgroundColor = "darkslategray";
+      muteBtn.style.color = "white";
+      muted = true;
+    } else {
+      muteBtn.innerText = "Mute";
+      muteBtn.style.backgroundColor = "lightgray";
+      muteBtn.style.color = "black";
+      muted = false;
     }
+  }
 }
 
 function handleCameraClick() {
-    if (myVideo) {
-        myVideo
-            .getVideoTracks()
-            .forEach((track) => (track.enabled = !track.enabled));
-        if (!cameraOff) {
-            cameraBtn.innerText = "Turn Camera On";
-            cameraBtn.style.backgroundColor = "darkslategray";
-            cameraBtn.style.color = "white";
-            cameraOff = true;
-        } else {
-            cameraBtn.innerText = "Turn Camera Off";
-            cameraBtn.style.backgroundColor = "lightgray";
-            cameraBtn.style.color = "black";
-            cameraOff = false;
-        }
+  if (myVideo) {
+    myVideo
+      .getVideoTracks()
+      .forEach((track) => (track.enabled = !track.enabled));
+    if (!cameraOff) {
+      cameraBtn.innerText = "Turn Camera On";
+      cameraBtn.style.backgroundColor = "darkslategray";
+      cameraBtn.style.color = "white";
+      cameraOff = true;
+    } else {
+      cameraBtn.innerText = "Turn Camera Off";
+      cameraBtn.style.backgroundColor = "lightgray";
+      cameraBtn.style.color = "black";
+      cameraOff = false;
     }
+  }
 }
 
 async function handleCameraChange() {
-    await getVideo(camerasSelect.value);
-    if (myPeerConnection) {
-        const videoTrack = myVideo.getVideoTracks()[0];
-        const videoSender = myPeerConnection
-            .getSenders()
-            .find((sender) => sender.track.kind === "video");
-        videoSender.replaceTrack(videoTrack);
-    }
+  await getVideo(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myVideo.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
@@ -141,23 +141,23 @@ const welcome = document.getElementById("welcome");
 const welcomeForm = document.getElementById("room-name");
 
 async function initCall() {
-    main.hidden = true;
-    room.style.display = "flex";
-    await getVideo();
-    await getAudio();
-    makeConnection();
+  main.hidden = true;
+  room.style.display = "flex";
+  await getVideo();
+  await getAudio();
+  makeConnection();
 }
 
 async function handleWelcomeSubmit(event) {
-    event.preventDefault();
-    const input = welcomeForm.querySelector("input");
-    await initCall();
-    socket.emit("join_room", input.value);
-    roomName = input.value;
-    if (main.hidden === true) {
-        title.innerText = `  Noom: ${roomName}  `;
-    }
-    input.value = "";
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  await initCall();
+  socket.emit("join_room", input.value);
+  roomName = input.value;
+  if (main.hidden === true) {
+    title.innerText = `  Noom: ${roomName}  `;
+  }
+  input.value = "";
 }
 
 welcomeForm.addEventListener("submit", handleWelcomeSubmit);
@@ -165,41 +165,41 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 // Socket Code
 
 socket.on("welcome", async () => {
-    myDataChannel = myPeerConnection.createDataChannel("chat");
-    myDataChannel.addEventListener("message", handlePeerChat);
-    console.log("made data channel");
-    const offer = await myPeerConnection.createOffer();
-    myPeerConnection.setLocalDescription(offer);
-    console.log("sent the offer");
-    socket.emit("offer", offer, roomName);
+  myDataChannel = myPeerConnection.createDataChannel("chat");
+  myDataChannel.addEventListener("message", handlePeerChat);
+  console.log("made data channel");
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
 });
 
 socket.on("offer", async (offer) => {
-    myPeerConnection.addEventListener("datachannel", (event) => {
-        myDataChannel = event.channel;
-        myDataChannel.addEventListener("message", handlePeerChat);
-    });
-    console.log("received the offer");
-    myPeerConnection.setRemoteDescription(offer);
-    const answer = await myPeerConnection.createAnswer();
-    myPeerConnection.setLocalDescription(answer);
-    socket.emit("answer", answer, roomName);
-    console.log("sent the answer");
+  myPeerConnection.addEventListener("datachannel", (event) => {
+    myDataChannel = event.channel;
+    myDataChannel.addEventListener("message", handlePeerChat);
+  });
+  console.log("received the offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+  console.log("sent the answer");
 });
 
 socket.on("answer", (answer) => {
-    console.log("received the answer");
-    myPeerConnection.setRemoteDescription(answer);
+  console.log("received the answer");
+  myPeerConnection.setRemoteDescription(answer);
 });
 
 socket.on("ice", (ice) => {
-    console.log("received candidate")
-    myPeerConnection.addIceCandidate(ice);
-    state.innerText = "🟢 Connected";
-    if (!peerFace.srcObject) {
-        peerFace.style.backgroundImage = "url(public/img/unknown.png)";
-        peerFace.style.backgroundSize = "contain";
-    }
+  console.log("received candidate");
+  myPeerConnection.addIceCandidate(ice);
+  state.innerText = "🟢 Connected";
+  if (!peerFace.srcObject) {
+    peerFace.style.backgroundImage = "url(public/img/unknown.png)";
+    peerFace.style.backgroundSize = "contain";
+  }
 });
 
 socket.on("exit_room", handlePeerExitRoom);
@@ -209,45 +209,45 @@ socket.on("peer_disconnecting", handlePeerExitRoom);
 // RTC Code
 
 function makeConnection() {
-    myPeerConnection = new RTCPeerConnection({
-        iceServers: [
-            {
-                urls: [
-                    "stun:stun.l.google.com:19302",
-                    "stun:stun1.l.google.com:19302",
-                    "stun:stun2.l.google.com:19302",
-                    "stun:stun3.l.google.com:19302",
-                    "stun:stun4.l.google.com:19302",
-                ]
-            }
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
         ],
-    });
-    myPeerConnection.addEventListener("icecandidate", handleIce);
-    myPeerConnection.addEventListener("track", handleAddTrack);
-    if (myVideo) {
-        myVideo
-            .getVideoTracks()
-            .forEach((track) => myPeerConnection.addTrack(track, myVideo));
-    }
-    if (myAudio) {
-        myAudio
-            .getAudioTracks()
-            .forEach((track) => myPeerConnection.addTrack(track, myAudio));
-    }
+      },
+    ],
+  });
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("track", handleAddTrack);
+  if (myVideo) {
+    myVideo
+      .getVideoTracks()
+      .forEach((track) => myPeerConnection.addTrack(track, myVideo));
+  }
+  if (myAudio) {
+    myAudio
+      .getAudioTracks()
+      .forEach((track) => myPeerConnection.addTrack(track, myAudio));
+  }
 }
 
 function handleIce(data) {
-    console.log("sent candidate");
-    socket.emit("ice", data.candidate, roomName);
-    state.innerText = "🟢 Connected";
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);
+  state.innerText = "🟢 Connected";
 }
 
 async function handleAddTrack(data) {
-    if (data.track.kind === "video") {
-        peerFace.srcObject = await data.streams[0];
-    } else {
-        peerVoice.srcObject = await data.streams[0];
-    }
+  if (data.track.kind === "video") {
+    peerFace.srcObject = await data.streams[0];
+  } else {
+    peerVoice.srcObject = await data.streams[0];
+  }
 }
 
 // chat & room exit
@@ -255,90 +255,86 @@ async function handleAddTrack(data) {
 const chat = document.getElementById("chat");
 const chatForm = document.getElementById("chat-form");
 const chatInput = chatForm.querySelector("#chat-input");
-const exitForm = document.getElementById("exit-form")
+const exitForm = document.getElementById("exit-form");
 const exitRoomBtn = exitForm.querySelector("#exit-room-button");
 
 function handleMyChat(event) {
-    event.preventDefault();
-    const div = document.createElement("div");
-    const p = document.createElement("p");
-    const value = chatInput.value;
-    div.className = "my-div";
-    p.className = "my-chat";
-    p.innerText = value;
+  event.preventDefault();
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  const value = chatInput.value;
+  div.className = "my-div";
+  p.className = "my-chat";
+  p.innerText = value;
 
-    if (myPeerConnection.iceConnectionState === "connected") {
-        myDataChannel.send(value);
-    }
-    
-    div.append(p);
-    chat.append(div);
-    chatInput.value = "";
-    chat.scrollTop = chat.scrollHeight;
+  if (myPeerConnection.iceConnectionState === "connected") {
+    myDataChannel.send(value);
+  }
+
+  div.append(p);
+  chat.append(div);
+  chatInput.value = "";
+  chat.scrollTop = chat.scrollHeight;
 }
 
 function handlePeerChat(event) {
-    const div = document.createElement("div");
-    const p = document.createElement("p");
-    const value = event.data;
-    div.className = "peer-div";
-    p.className = "peer-chat";
-    p.innerText = value;
-    div.append(p);
-    chat.append(div);
-    chat.scrollTop = chat.scrollHeight;
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  const value = event.data;
+  div.className = "peer-div";
+  p.className = "peer-chat";
+  p.innerText = value;
+  div.append(p);
+  chat.append(div);
+  chat.scrollTop = chat.scrollHeight;
 }
 
 function handleExitRoom() {
-    title.innerText = "";
-    state.innerText = "🟠 Waiting";
-    main.hidden = false;
-    room.style.display = "none";
-    muteBtn.innerText = "Mute";
-    muted = false;
-    cameraBtn.innerText = "Turn Camera Off";
-    cameraOff = false;
-    if (myVideo) {
-        myVideo
-            .getVideoTracks()
-            .forEach((track) => (track.enabled = false));
-    }
-    if (myAudio) {
-        myAudio
-            .getAudioTracks()
-            .forEach((track) => (track.enabled = false));
-    }
-    socket.emit("exit_room", roomName);
-    peerFace.srcObject = null;
-    peerVoice.srcObject = null;
-    while (chat.childNodes.length) {
-        chat.removeChild(chat.childNodes[0]);
-    }
+  title.innerText = "";
+  state.innerText = "🟠 Waiting";
+  main.hidden = false;
+  room.style.display = "none";
+  muteBtn.innerText = "Mute";
+  muted = false;
+  cameraBtn.innerText = "Turn Camera Off";
+  cameraOff = false;
+  if (myVideo) {
+    myVideo.getVideoTracks().forEach((track) => (track.enabled = false));
+  }
+  if (myAudio) {
+    myAudio.getAudioTracks().forEach((track) => (track.enabled = false));
+  }
+  socket.emit("exit_room", roomName);
+  peerFace.srcObject = null;
+  peerVoice.srcObject = null;
+  while (chat.childNodes.length) {
+    chat.removeChild(chat.childNodes[0]);
+  }
 }
 
 function handleMyExitRoom(event) {
-    event.preventDefault();
-    handleExitRoom();
-    notice.classList.remove("disappear");
-    notice.hidden = false;
-    notice.innerText = "방에서 나왔습니다.";
-    disapper();
-    setTimeout((() => notice.hidden = true), 3000);
+  event.preventDefault();
+  handleExitRoom();
+  notice.classList.remove("disappear");
+  notice.hidden = false;
+  notice.innerText = "방에서 나왔습니다.";
+  disapper();
+  setTimeout(() => (notice.hidden = true), 3000);
 }
 
 function handlePeerExitRoom() {
-    handleExitRoom();
-    notice.classList.remove("disappear");
-    notice.hidden = false;
-    notice.innerText = "상대방이 방을 나갔습니다.";
-    disapper();
-    setTimeout((() => notice.hidden = true), 3000);
+  handleExitRoom();
+  notice.classList.remove("disappear");
+  notice.hidden = false;
+  notice.innerText = "상대방이 방을 나갔습니다.";
+  disapper();
+  setTimeout(() => (notice.hidden = true), 3000);
 }
 
 function disapper() {
-    setTimeout(() => {
-        notice.classList.add("disappear");
-    }, 2000);
+  setTimeout(() => {
+    notice.classList.add("disappear");
+  }, 2000);
 }
 
 chatForm.addEventListener("submit", handleMyChat);
--- src/public/js/background.js
+++ src/public/js/background.js
@@ -1,12 +1,12 @@
 "use strict";
 
 const images = [
-    "puppy.jpg",
-    "cat.jpg",
-    "lion.jpg",
-    "penguin.jpg",
-    "polar_bear.jpg",
-]
+  "puppy.jpg",
+  "cat.jpg",
+  "lion.jpg",
+  "penguin.jpg",
+  "polar_bear.jpg",
+];
 
 const bgImg = document.getElementById("bgImg");
 let currentBackgroundImg = "url(public/img/puppy.jpg)";
@@ -14,19 +14,20 @@ bgImg.style.backgroundImage = currentBackgroundImg;
 const backgroundSelect = document.getElementById("background-img-change");
 
 function handleBgImgChange() {
-    const selected = backgroundSelect.options[backgroundSelect.selectedIndex].text;
-    currentBackgroundImg = `url(public/img/${selected}.jpg)`
-    bgImg.style.backgroundImage = currentBackgroundImg;
+  const selected =
+    backgroundSelect.options[backgroundSelect.selectedIndex].text;
+  currentBackgroundImg = `url(public/img/${selected}.jpg)`;
+  bgImg.style.backgroundImage = currentBackgroundImg;
 }
 
 backgroundSelect.addEventListener("input", handleBgImgChange);
 
 images.forEach((img) => {
-    const option = document.createElement("option");
-    option.value = img;
-    option.innerText = img.slice(0, -4);
-    if (bgImg.style.backgroundImage === `url(public/img/${img})`) {
-        option.selected = true;
-    }
-    backgroundSelect.appendChild(option);
-})
+  const option = document.createElement("option");
+  option.value = img;
+  option.innerText = img.slice(0, -4);
+  if (bgImg.style.backgroundImage === `url(public/img/${img})`) {
+    option.selected = true;
+  }
+  backgroundSelect.appendChild(option);
+});
